6bc7d88d6e6993b0bbfafa4d9f8f1202148fe072
farizziraf <farizziraf3105@gmail.com>
2023-12-05 18:28:58 +0700

    createUser

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -4,20 +4,22 @@ const login = async (req, res) => {
 	res.status(200).json({ message: "Login" });
 };
 
-const register = async (req, res) => {
-	const { name, email, password } = req.body;
+const createUser = async (req, res) => {
+    const { name, email, pass, age, gender, weight, height, activity_freq, fitness_level, primary_goal, equipment } = req.body;
 
-	try {
-		const insertQuery =
-			"INSERT INTO users (name, email, pass) VALUES ($1, $2, $3) RETURNING *";
-		const values = [name, email, password];
-		const result = await client.query(insertQuery, values);
+    try {
+        const insertQuery =
+            "INSERT INTO users (name, email, pass, age, gender, weight, height, activity_freq, fitness_level, primary_goal, equipment) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11) RETURNING *";
 
-		res.status(201).json(result.rows[0]);
-	} catch (error) {
-		console.error(error);
-		res.status(500).json({ error: "Internal Server Error" });
-	}
+        const values = [name, email, pass, age, gender, weight, height, activity_freq, fitness_level, primary_goal, equipment];
+
+        const result = await client.query(insertQuery, values);
+
+        res.status(201).json(result.rows[0]);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
 };
 
 const profile = async (req, res) => {
