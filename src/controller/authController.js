45525f0e76157ac8afffc26aa5271c18b5971365
adeliaaaa <monicadelia88@gmail.com>
2023-12-06 11:39:06 +0700

    fix: register function name

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -5,25 +5,49 @@ const login = async (req, res) => {
 };
 
 const createUser = async (req, res) => {
-    const { name, email, pass, age, gender, weight, height, activity_freq, fitness_level, primary_goal, equipment } = req.body;
+	const {
+		name,
+		email,
+		pass,
+		age,
+		gender,
+		weight,
+		height,
+		activity_freq,
+		fitness_level,
+		primary_goal,
+		equipment,
+	} = req.body;
 
-    try {
-        const insertQuery =
-            "INSERT INTO users (name, email, pass, age, gender, weight, height, activity_freq, fitness_level, primary_goal, equipment) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11) RETURNING *";
+	try {
+		const insertQuery =
+			"INSERT INTO users (name, email, pass, age, gender, weight, height, activity_freq, fitness_level, primary_goal, equipment) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11) RETURNING *";
 
-        const values = [name, email, pass, age, gender, weight, height, activity_freq, fitness_level, primary_goal, equipment];
+		const values = [
+			name,
+			email,
+			pass,
+			age,
+			gender,
+			weight,
+			height,
+			activity_freq,
+			fitness_level,
+			primary_goal,
+			equipment,
+		];
 
-        const result = await client.query(insertQuery, values);
+		const result = await client.query(insertQuery, values);
 
-        res.status(201).json(result.rows[0]);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: "Internal Server Error" });
-    }
+		res.status(201).json(result.rows[0]);
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ error: "Internal Server Error" });
+	}
 };
 
 const profile = async (req, res) => {
 	res.status(200).json({ message: "Profile" });
 };
 
-module.exports = { login, register, profile };
+module.exports = { login, createUser, profile };
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -3,7 +3,7 @@ const authRouter = express.Router();
 const authHandler = require("../controller/authController");
 
 authRouter.post("/login", authHandler.login);
-authRouter.post("/register", authHandler.register);
+authRouter.post("/register", authHandler.createUser);
 authRouter.get("/profile", authHandler.profile);
 
 module.exports = authRouter;
