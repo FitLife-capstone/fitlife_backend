f06d03e0e9255057466e29cb0469b5b98107659e
adeliaaaa <monicadelia88@gmail.com>
2023-12-06 15:58:24 +0700

    feat: api get all exercise, detail exercise

--- src/controller/activityController.js
+++ src/controller/activityController.js
@@ -1,11 +1,40 @@
 const { client } = require("../db");
 
 const getAllActivity = async (req, res) => {
-	res.status(200).json({ message: "get all activities" });
+	const result = {};
+
+	try {
+		const query = "SELECT * FROM exercise";
+
+		const queryResult = await client.query(query);
+
+		result["error"] = false;
+		result["message"] = "Success";
+		result["data"] = queryResult.rows;
+
+		res.status(201).json(result);
+	} catch (error) {
+		res.status(500).json({ error: "Internal Server Error" });
+	}
 };
 
 const getActivity = async (req, res) => {
-	res.status(200).json({ message: "get detail acitivity" });
+	const exercise_id = req.params.id;
+	const result = {};
+
+	try {
+		const query = `SELECT * FROM exercise WHERE exercise_id = ${exercise_id}`;
+
+		const queryResult = await client.query(query);
+
+		result["error"] = false;
+		result["message"] = "Success";
+		result["data"] = queryResult.rows[0];
+
+		res.status(201).json(result);
+	} catch (error) {
+		res.status(500).json({ error: "Internal Server Error" });
+	}
 };
 
 module.exports = { getAllActivity, getActivity };
--- src/routes/activityRoutes.js
+++ src/routes/activityRoutes.js
@@ -3,6 +3,6 @@ const activityRoutes = express.Router();
 const activityHandler = require("../controller/activityController");
 
 activityRoutes.get("/activities", activityHandler.getAllActivity);
-activityRoutes.get("/activity", activityHandler.getActivity);
+activityRoutes.get("/activity/:id", activityHandler.getActivity);
 
 module.exports = activityRoutes;
