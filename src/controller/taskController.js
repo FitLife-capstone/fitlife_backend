2953955f95e630ff02da4b0e78148f4b0cf23300
adeliaaaa <monicadelia88@gmail.com>
2023-12-07 13:50:34 +0700

    feat: create task admin

--- src/controller/taskController.js
+++ src/controller/taskController.js
@@ -8,6 +8,39 @@ const getAllTask = async (req, res) => {
 
 		const queryResult = await client.query(query);
 
+		if (queryResult.rows.length == 0) {
+			result["error"] = true;
+			result["message"] = "No active challenge";
+			res.status(400).json(result);
+			return;
+		}
+
+		result["error"] = false;
+		result["message"] = "Success";
+		result["data"] = queryResult.rows;
+
+		res.status(201).json(result);
+	} catch (error) {
+		res.status(500).json({ error: "Internal Server Error" });
+	}
+};
+
+const getAllActiveTask = async (req, res) => {
+	const result = {};
+
+	try {
+		const query =
+			"SELECT * FROM task WHERE end_date >= CURRENT_DATE  ORDER BY created_date DESC";
+
+		const queryResult = await client.query(query);
+
+		if (queryResult.rows.length == 0) {
+			result["error"] = true;
+			result["message"] = "No active challenge";
+			res.status(400).json(result);
+			return;
+		}
+
 		result["error"] = false;
 		result["message"] = "Success";
 		result["data"] = queryResult.rows;
@@ -45,7 +78,38 @@ const getTask = async (req, res) => {
 };
 
 const createTask = async (req, res) => {
-	res.status(200).json({ message: "create task" });
+	const result = {};
+	const token = req?.headers["authorization"];
+
+	const { category, task_name, task_desc, start_at, end_at } = req.body;
+
+	if (!category || !task_name || !task_desc || !start_at || !end_at) {
+		result["error"] = true;
+		result["message"] = "Missing required fields";
+		res.status(400).json(result);
+		return;
+	} else if (start_at > end_at) {
+		result["error"] = true;
+		result["message"] = "Start date must be before end date";
+		res.status(400).json(result);
+		return;
+	} else {
+		try {
+			const insertQuery =
+				"INSERT INTO task (category, task_name, task_desc, created_date, end_date) VALUES ($1, $2, $3, $4, $5) RETURNING *";
+
+			const values = [category, task_name, task_desc, start_at, end_at];
+
+			const resultQuery = await client.query(insertQuery, values);
+
+			result["error"] = false;
+			result["message"] = "Task created";
+			result["data"] = resultQuery.rows[0];
+			res.status(201).json(result);
+		} catch (error) {
+			res.status(500).json({ error: "Internal Server Error" });
+		}
+	}
 };
 
-module.exports = { getAllTask, getTask, createTask };
+module.exports = { getAllTask, getTask, createTask, getAllActiveTask };
--- src/routes/taskRoutes.js
+++ src/routes/taskRoutes.js
@@ -3,6 +3,7 @@ const taskRoutes = express.Router();
 const taskHandler = require("../controller/taskController");
 
 taskRoutes.get("/tasks", taskHandler.getAllTask);
+taskRoutes.get("/active-tasks", taskHandler.getAllActiveTask);
 taskRoutes.get("/task/:id", taskHandler.getTask);
 taskRoutes.post("/create-task", taskHandler.createTask);
 
