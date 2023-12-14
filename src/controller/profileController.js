318892dcd3ef05740443f0e9d60fa0361eb223ab
adeliaaaa <monicadelia88@gmail.com>
2023-12-14 21:09:52 +0700

    feat: get user exercise & admin

--- src/controller/profileController.js
+++ src/controller/profileController.js
@@ -12,11 +12,35 @@ const getUserDetail = async (req, res) => {
 };
 
 const getUserExercise = async (req, res) => {
-	res.status(200).json({ message: "user exercise" });
+	if (req.user && req.user.userId) {
+		const userId = req.user.userId;
+		try {
+			const selectQuery = `SELECT * FROM user_exercise WHERE user_id = ${userId}`;
+			const result = await client.query(selectQuery);
+			res.status(200).json(result.rows);
+		} catch (error) {
+			console.error(error);
+			res.status(500).json({ error: "Internal Server Error" });
+		}
+	} else {
+		res.status(401).json({ error: "Unauthorized" });
+	}
 };
 
 const getUserTask = async (req, res) => {
-	res.status(200).json({ message: "user task" });
+	if (req.user && req.user.userId) {
+		const userId = req.user.userId;
+		try {
+			const selectQuery = `SELECT * FROM user_task WHERE user_id = ${userId}`;
+			const result = await client.query(selectQuery);
+			res.status(200).json(result.rows);
+		} catch (error) {
+			console.error(error);
+			res.status(500).json({ error: "Internal Server Error" });
+		}
+	} else {
+		res.status(401).json({ error: "Unauthorized" });
+	}
 };
 
 module.exports = { getUserDetail, getUserExercise, getUserTask };
--- src/routes/profileRoutes.js
+++ src/routes/profileRoutes.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const profileRouter = express.Router();
 const profileHandler = require("../controller/profileController");
+const middleware = require("../middleware/auth");
 
 profileRouter.get("/user-detail", profileHandler.getUserDetail);
-profileRouter.get("/user-exercise", profileHandler.getUserExercise);
-profileRouter.get("/user-task", profileHandler.getUserTask);
+profileRouter.get(
+	"/user-exercise",
+	middleware.authMiddleware,
+	profileHandler.getUserExercise
+);
+profileRouter.get(
+	"/user-task",
+	middleware.authMiddleware,
+	profileHandler.getUserTask
+);
 
 module.exports = profileRouter;
