dc9448ee3c6c4fc723b057adbcdf5b1d12b1fa72
adeliaaaa <monicadelia88@gmail.com>
2023-12-19 15:20:20 +0700

    feat: add cors

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 const authRoutes = require("./routes/authRoutes");
 const profileRoutes = require("./routes/profileRoutes");
@@ -9,6 +10,8 @@ const pickedExerciseRoutes = require("./routes/pickedExerciseRoutes");
 const pickedTaskRoutes = require("./routes/pickedTaskRoutes");
 
 const PORT = process.env.PORT || 5000;
+
+app.use(cors());
 
 app.use(express.json());
 
