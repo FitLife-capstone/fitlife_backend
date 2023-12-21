f9e6e89ac0f392070aac4c6928aa3c808ae3fc96
farizziraf <farizziraf3105@gmail.com>
2023-12-21 13:07:55 +0700

    bug fixed: upload cloud storage

--- src/controller/pickedTaskController.js
+++ src/controller/pickedTaskController.js
@@ -7,7 +7,7 @@ const {
   ErrorInternalServer,
   ErrorUnauthorized,
 } = require("../common/commonResponse");
-const path = require("path");
+const fs = require("fs");
 
 const storage = new Storage({
   projectId: process.env.PROJECT_ID,
@@ -16,61 +16,61 @@ const storage = new Storage({
 
 const bucketName = process.env.BUCKETNAME;
 
+function deleteImage(gcsFilePath) {
+  const bucket = storage.bucket(bucketName);
+  const file = bucket.file(gcsFilePath);
+
+  file.delete().then(() => {
+    console.log(`Image deleted successfully from Google Cloud Storage: ${gcsFilePath}`);
+  }).catch((err) => {
+    console.error(`Error deleting image from Google Cloud Storage: ${err}`);
+  });
+}
+
 const submitTask = async (req, res) => {
   if (req.user && req.user.userId) {
     const { task_id, rate } = req.body;
     const user_id = req.user.userId;
 
     try {
-      let query = `SELECT * FROM user_task WHERE task_id = ${task_id} AND user_id = ${user_id}`;
-      let queryResult = await client.query(query);
-
-      if (queryResult.rows.length > 0) {
-        res.status(400).json({
-          error: true,
-          message: "Task already submitted",
-        });
-        return;
-      }
-
       if (req.file) {
+        const imgPath = req.file.path;
         const timestamp = new Date().getTime();
         const uniqueFilename = `${timestamp}-${req.file.originalname}`;
-        const file = storage
-          .bucket(bucketName)
-          .file(`src/uploads/task/${uniqueFilename}`);
+        const targetPath = `src/uploads/task/${uniqueFilename}`;
 
-        const stream = file.createWriteStream({
-          metadata: {
-            contentType: req.file.mimetype,
-          },
+        const bucket = storage.bucket(bucketName);
+        const file = bucket.file(targetPath);
+
+        await file.save(fs.createReadStream(imgPath), {
+          metadata: { contentType: req.file.mimetype },
         });
 
-        stream.on("error", (err) => {
-          console.error(`Error uploading to Cloud Storage: ${err}`);
-          return res.status(500).json(ErrorUploadingFile);
-        });
+        deleteImage(imgPath);
 
-        stream.on("finish", async () => {
-          const imgURL = `https://storage.googleapis.com/${bucketName}/src/uploads/task/${uniqueFilename}`;
+        let query = `SELECT * FROM user_task WHERE task_id = ${task_id} AND user_id = ${user_id}`;
+        let queryResult = await client.query(query);
 
-          query = `INSERT INTO user_task (task_id, user_id, rate, img, status) VALUES (${task_id}, ${user_id}, ${rate}, '${imgURL}', 'PENDING') RETURNING *`;
+        if (queryResult.rows.length > 0) {
+          res.status(400).json({
+            error: true,
+            message: 'Task already submitted',
+          });
+          return;
+        } else {
+          query = `INSERT INTO user_task (task_id, user_id, rate, img, status) VALUES (${task_id}, ${user_id}, ${rate}, '${file.publicUrl()}', 'PENDING') RETURNING *`;
           queryResult = await client.query(query);
-
-          console.log(`Image uploaded successfully: ${imgURL}`);
 
           res.status(201).json({
             error: false,
-            message: "Task submitted",
+            message: 'Task submitted',
             data: queryResult.rows[0],
           });
-        });
-
-        stream.end(req.file.buffer);
+        }
       } else {
         res.status(400).json({
           error: true,
-          message: "Missing required fields",
+          message: 'Missing required fields',
         });
       }
     } catch (error) {
